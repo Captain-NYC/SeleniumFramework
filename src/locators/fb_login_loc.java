eb01fda0af48d1c118fafd8ef76e612e96621456
Golam Sadek <sadekny@gmail.com>
2018-11-19 11:55:44 -0500

    Framework

---
+++ src/locators/fb_login_loc.java
@@ -0,0 +1,11 @@
+package locators;
+
+public class fb_login_loc {
+
+	public String emailPhone 	 	= "//input[@id='email']";
+	public String password 			= "//input[@type='password']";
+	public String loginButton 		= "//*[@id='u_0_3']";
+	
+	public String signUpTextLoc		= "/html/body/div[1]/div[3]/div[1]/div/div/div/div/div[2]/h2";
+	
+}
